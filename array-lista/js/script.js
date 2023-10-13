4f8bedae1d8be2b4930cefa79167a6cfc78d0b61
ericaostini <ericaostini14@gmail.com>
2023-10-13 15:49:00 +0200

    display messagge "Trovato" if "cocomero" is in array "frigorifero"

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -13,3 +13,19 @@ const frigorifero = [
 //Inserisco nell'array il valore "pesca"
 frigorifero.push("pesca");
 console.log(frigorifero);
+
+//verificare se nell'array di frutta c'è il "cocomero"
+const alertText = document.querySelector(".alert");
+let textEl;
+let frutto;
+for(let i = 0; i < frigorifero.length -1; i++){
+    frutto = frigorifero[i];
+    console.log(frutto);
+    if (frutto === "cocomero"){
+        textEl = "Trovato! Devo solo preparare il cocktail."
+        console.log(textEl);
+    } else{
+        frutto += 1;
+    }
+}
+alertText.innerHTML = textEl;
