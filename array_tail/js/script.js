c216029e0df0d75b13b074d50dca1e0ce55f0df1
ericaostini <ericaostini14@gmail.com>
2023-10-13 17:10:39 +0200

    add random number to the empty list

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -6,14 +6,17 @@ const elementi = document.getElementById("elementi");
 const buttonEl = document.querySelector("button");
 
 let numberList;
+let listArray = [];
 buttonEl.addEventListener("click", function(){
     let numeroEl = elementi.value;
     console.log(numeroEl);
-    for(let i = 1; i < numeroEl; i++){
+    for(let i = 0; i < numeroEl; i++){
         numberList = getRndInteger(0,100);
         console.log(numberList);
-
+        listArray.splice(0,0,numberList);
+        console.log(listArray);
     }
+
 })
 
 // let listEl = [];
