fc41be04045b6b86ef1a81790e91bc2b508c2612
ericaostini <ericaostini14@gmail.com>
2023-10-13 15:18:34 +0200

    creation of fruit list

---
+++ array-lista/js/script.js
@@ -0,0 +1,12 @@
+const frigorifero = [
+    'banana',
+     'mela',
+     'pera',
+     'ciliegia',
+     'arancia',
+     'mandarino',
+     'cocomero', 
+     'limone',
+     'fragola'
+]
+console.log(frigorifero);
